8f97de5c5e26c193a66a85af92412d808e355953
PFLORE <pfh_17@hotmail.com>
2022-02-16 10:04:12 -0500

    Subiendo cambios

---
+++ Jazani.ICL.Servicios/ProcedimientoGeneral/Dtos/ProcedimientoDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jazani.ICL.Servicios.ProcedimientoGeneral.Dtos
+{
+    public class ProcedimientoDto
+    {
+        public String Id { get; set; }
+        public String Nombre { get; set; }
+        public String Descripcion { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public TipoProcedimientoDto TipProcedimiento { get; set; }
+        public List<ActividadDto> Actividad { get; set; }
+
+    }
+}
