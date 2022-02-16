e16461255fa5f86aa253c957056b7fe80752feb8
David Vera <dveracabanillas@gmail.com>
2022-02-15 19:56:48 -0500

    listado de la tabla SECTOR

---
+++ Jazani.ICL.Datos/General/Repositorio/Abstracciones/ISectorRepositorio.cs
@@ -0,0 +1,19 @@
+﻿using Jazani.ICL.Datos.Auth.Entidades;
+using Jazani.ICL.Datos.General.Entidades;
+using Jazani.ICL.Datos.Infraestructura.Repositorios.Abstracciones;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jazani.ICL.Datos.General.Repositorio.Abstracciones
+{
+    public interface ISectorRepositorio : IICLRepositorio<Sector, long>
+    {
+        //Task<Sector> BuscarPorCodigoAsync(string codigo);
+        Task<Sector> BuscarPorCodigoAsync(string codigo);
+        Task<List<Sector>> ListarAsync();
+        //Task<Tuple<List<Sector>, int>> ListarPaginadoAsync(string orden, int start, int length, string nombre = null);
+    }
+}
