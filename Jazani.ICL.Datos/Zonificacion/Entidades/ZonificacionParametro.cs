42fb880f52512e5f67931d7f7e6c6166f6a0ccd8
yordysh <yordysh28@gmail.com>
2022-02-16 21:12:17 -0500

    ZonificacionParametro agregando atributos

--- Jazani.ICL.Datos/Zonificacion/Entidades/ZonificacionParametro.cs
+++ Jazani.ICL.Datos/Zonificacion/Entidades/ZonificacionParametro.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Jazani.ICL.Datos.General.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,38 @@ namespace Jazani.ICL.Datos.Zonificacion.Entidades
 {
     public class ZonificacionParametro
     {
+        public long Id { get; set; }
+        public int IdZonificacionParametro { get; set; }
+        public string Codigo { get; set; }
+        public string Zonificacion { get; set; }
+        public string Abreviatura { get; set; }
+        public string CodigoUbigeo { get; set; }
+        public int IdTipoNorma { get; set; }
+        public string NumeroNormativa { get; set; }
+        public int IdSector { get; set; }
+        public string DensidadNeta { get; set; }
+        public string AreaLoteMinNormativo { get; set; }
+        public string FrenteLoteMinNormativo { get; set; }
+        public string AlturaMaxEdificacionPiso { get; set; }
+        public string AlturaMaxEdificacion { get; set; }
+        public string PorcentajeMinAreaLibre { get; set; }
+        public string UsoPermitido { get; set; }
+        public string RequiereEstacionamiento { get; set; }
+        public string CoeficienteEdificacion { get; set; }
+        public string UsoCompatibles { get; set; }
+        public string Nota { get; set; }
+        public string Observacion { get; set; }
+        public int EstadoCulminado { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public int Estado { get; set; }
+        public virtual Ubigeo Ubigeo { get; set; }
+        //public virtual TipoNorma TipoNorma { get; set; }
+        public virtual Sector Sector { get; set; }
+
+        public ZonificacionParametro()
+        {
+            FechaRegistro = DateTime.UtcNow;
+            Estado = 1;
+        }
     }
 }
